37b5a53aa6e461a80a308a161e14777ad0a89730
Nikola Stankovic <nikola.stankovic@umb.ch>
2020-03-20 14:34:32 +0100

    initial commit

---
+++ src/main/java/com/example/helloworld/model/Greeting.java
@@ -0,0 +1,13 @@
+package com.example.helloworld.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class Greeting implements Serializable {
+    private final long id;
+    private final String content;
+}
