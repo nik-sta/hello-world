da306d8e4f4f4ebcd643d71600e097d59c4823a1
itsmefox <patrick.boesch@viascom.ch>
2022-06-24 10:26:48 +0200

    feat: try to make it smaller

--- docker/run_application.sh
+++ docker/run_application.sh
@@ -4,4 +4,4 @@ JAVA_OPTS="-Dfile.encoding=UTF-8 -Duser.timezone=UTC"
 PORT="8080"
 
 cd /srv/"$APP" || exit
-java $JAVA_OPTS org.springframework.boot.loader.JarLauncher --bind 0.0.0.0:$PORT
+/opt/java/bin/java $JAVA_OPTS org.springframework.boot.loader.JarLauncher --bind 0.0.0.0:$PORT
